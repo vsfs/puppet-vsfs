28aa99e400c50c589ce6cce717bceec3b0ad7a8d
Lei Xu <eddyxu@gmail.com>
2013-04-17 04:30:49 -0500

    * Initialize puppet configure

---
+++ manifests/init.pp
@@ -0,0 +1,83 @@
+# == Class: vsfs
+#
+# Install VSFS Development Environment.
+#
+# === Authors
+#
+# Lei Xu <eddyxu@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2013 Lei Xu
+#
+class vsfs {
+  include bdb
+  include boost
+  include cpp
+  include google
+  include leveldb
+  include libevent
+  include thrift
+
+  case $operatingsystem {
+    centos, Scientific: {
+      $git = 'git'
+      $vim = 'vim-enhanced'
+      $pkgconfig = 'pkgconfig'
+      $libattr = 'libattr-devel'
+      $libfuse = 'fuse-devel'
+    }
+    ubuntu: {
+      $git = 'git-core'
+      $vim = 'vim'
+      $pkgconfig = 'pkg-config'
+      $libattr = 'libattr1-dev'
+      $libfuse = 'libfuse-dev'
+    }
+  }
+
+  package { 'git':
+    ensure => present,
+    name => $git,
+  }
+
+  package { [ 'autoconf', 'automake', 'cscope', 'ctags', 'curl', 'make', 'wget',
+      'libtool', 'gdb' ]:
+    ensure => present,
+  }
+
+  package { 'vim':
+    ensure => present,
+    name => $vim,
+  }
+
+  package { 'pkgconfig':
+    ensure => installed,
+    name => $pkgconfig,
+  }
+
+  package { ['fuse', $libfuse]:
+    ensure => installed,
+  }
+
+  package { 'libattr-devel':
+    ensure => installed,
+    name   => $libattr,
+  }
+
+  case $operatingsystem {
+    centos, Scientific: {
+      package { ['protobuf-devel', 'mysql++-devel', 'gperftools-devel']:
+        ensure  => installed,
+        require => Yumrepo["EPEL"],
+      }
+    }
+    ubuntu: {
+      package { ['libprotobuf-dev', 'libmysql++-dev', 'libgoogle-perftools-dev']:
+        ensure => installed,
+      }
+    }
+  }
+}
+
+include 'vsfs'

97b5fb49754315fd10d462c641386ce17bcc080d
Lei Xu <eddyxu@gmail.com>
2013-05-03 03:21:52 -0500

    - remove level db

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,6 +27,14 @@ class vsfs {
       $libattr = 'libattr-devel'
       $libfuse = 'fuse-devel'
       $libssl = 'openssl-devel'
+
+      $repo_url = 'http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm'
+
+      yumrepo { 'EPEL':
+        baseurl  => 'http://dl.fedoraproject.org/pub/epel/6/x86_64',
+        enabled  => 1,
+        gpgcheck => 0,
+      }
     }
     ubuntu: {
       $git = 'git-core'

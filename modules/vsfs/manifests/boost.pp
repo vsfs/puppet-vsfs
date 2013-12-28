d0f0da2d19a481553eb457ae70a127b4000678aa
Lei Xu <eddyxu@gmail.com>
2013-12-28 14:33:37 -0600

    * install boost-python

--- modules/vsfs/manifests/boost.pp
+++ modules/vsfs/manifests/boost.pp
@@ -16,7 +16,7 @@ class vsfs::boost {
 
       exec { 'configure_boost':
         cwd      => $cwd,
-        command  => "/bin/bash -c '${cwd}/bootstrap.sh -with-libraries=filesystem,system'",
+        command  => "/bin/bash -c '${cwd}/bootstrap.sh -with-libraries=filesystem,system,python'",
         require  => [Exec['download_boost'], Class['cpp']],
         provider => shell,
       }
@@ -29,7 +29,8 @@ class vsfs::boost {
       }
     }
     ubuntu: {
-      package { 'libboost-filesystem1.54-dev':
+      package { ['libboost-filesystem1.54-dev',
+                 'libboost-python1.54-dev']:
         ensure => installed,
       }
     }
